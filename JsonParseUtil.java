0f15d72058877f201e65e4ff631cba277d992528
kjkj9407 <985032636@qq.com>
2022-07-21 15:59:11 +0800

    Update JsonParseUtil.java

--- JsonParseUtil.java
+++ JsonParseUtil.java
@@ -1,10 +1,15 @@
-package com.example.demo;
-
+package com.fenbi.pumpkin.science.admin.server;
 /**
- * Created by 张城城 on 2018/6/11.
+ *
  */
+
+import com.fenbi.common.util.JsonUtils;
+import com.fenbi.pumpkin.science.admin.server.web.data.vo.question.SingleQuestionVO;
+import com.fenbi.pumpkin.science.refactor.data.question.SingleOption;
 import com.google.gson.*;
 import net.sf.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -18,10 +23,10 @@ public class JsonParseUtil {
      * @param uglyJSONString
      * @return
      */
-    public static String jsonFormatter(String uglyJSONString){
+    public static String jsonFormatter(String uglyJSONString) {
 
-        Map<String,Object> map = new HashMap<>();
-        parseJson2Map(map,uglyJSONString,null);
+        Map<String, Object> map = new HashMap<>();
+        parseJson2Map(map, uglyJSONString, null);
         JSONObject jsonObject = JSONObject.fromObject(map);
         uglyJSONString = jsonObject.toString();
 
@@ -33,34 +38,32 @@ public class JsonParseUtil {
         return prettyJsonString;
     }
 
-    public static void parseJson2Map(Map map,JsonObject jsonObject,String parentKey){
+    public static void parseJson2Map(Map map, JsonObject jsonObject, String parentKey) {
         for (Map.Entry<String, JsonElement> object : jsonObject.entrySet()) {
             String key = object.getKey();
             JsonElement value = object.getValue();
             String fullkey = (null == parentKey || parentKey.trim().equals("")) ? key : parentKey.trim() + "." + key;
             //判断对象的类型，如果是空类型则安装空类型处理
-            if (value.isJsonNull()){
-                map.put(fullkey,null);
+            if (value.isJsonNull()) {
+                map.put(fullkey, null);
                 continue;
-            //如果是JsonObject对象则递归处理
-            }else if (value.isJsonObject()){
-                parseJson2Map(map,value.getAsJsonObject(),fullkey);
-            //如果是JsonArray数组则迭代，然后进行递归
-            }else if (value.isJsonArray()){
+                //如果是JsonObject对象则递归处理
+            } else if (value.isJsonObject()) {
+                parseJson2Map(map, value.getAsJsonObject(), fullkey);
+                //如果是JsonArray数组则迭代，然后进行递归
+            } else if (value.isJsonArray()) {
                 JsonArray jsonArray = value.getAsJsonArray();
-                Iterator<JsonElement> iterator = jsonArray.iterator();
-                while (iterator.hasNext()) {
-                    JsonElement jsonElement1 = iterator.next();
-                    parseJson2Map(map, jsonElement1.getAsJsonObject(), fullkey);
+                for (int i = 0; i < jsonArray.size(); i++) {
+                    parseJson2Map(map, jsonArray.get(i).getAsJsonObject(), fullkey+"_"+i);
                 }
                 continue;
-             // 如果是JsonPrimitive对象则获取当中的值,则还需要再次进行判断一下
-            }else if (value.isJsonPrimitive()){
+                // 如果是JsonPrimitive对象则获取当中的值,则还需要再次进行判断一下
+            } else if (value.isJsonPrimitive()) {
                 try {
                     JsonElement element = new JsonParser().parse(value.getAsString());
-                    if (element.isJsonNull()){
-                        map.put(fullkey,value.getAsString());
-                    }else if (element.isJsonObject()) {
+                    if (element.isJsonNull()) {
+                        map.put(fullkey, value.getAsString());
+                    } else if (element.isJsonObject()) {
                         parseJson2Map(map, element.getAsJsonObject(), fullkey);
                     } else if (element.isJsonPrimitive()) {
                         JsonPrimitive jsonPrimitive = element.getAsJsonPrimitive();
@@ -72,13 +75,12 @@ public class JsonParseUtil {
                         }
                     } else if (element.isJsonArray()) {
                         JsonArray jsonArray = element.getAsJsonArray();
-                        Iterator<JsonElement> iterator = jsonArray.iterator();
-                        while (iterator.hasNext()) {
-                            parseJson2Map(map, iterator.next().getAsJsonObject(), fullkey);
+                        for (int i = 0; i < jsonArray.size(); i++) {
+                            parseJson2Map(map, jsonArray.get(i).getAsJsonObject(), fullkey+"_"+i);
                         }
                     }
-                }catch (Exception e){
-                    map.put(fullkey,value.getAsString());
+                } catch (Exception e) {
+                    map.put(fullkey, value.getAsString());
                 }
             }
         }
@@ -90,25 +92,25 @@ public class JsonParseUtil {
      * @param json
      * @param parentKey
      */
-    public static void parseJson2Map(Map map, String json, String parentKey){
+    public static void parseJson2Map(Map map, String json, String parentKey) {
         JsonElement jsonElement = new JsonParser().parse(json);
         if (jsonElement.isJsonObject()) {
             JsonObject jsonObject = jsonElement.getAsJsonObject();
-            parseJson2Map(map,jsonObject,parentKey);
+            parseJson2Map(map, jsonObject, parentKey);
             //传入的还是一个json数组
-        }else if (jsonElement.isJsonArray()){
+        } else if (jsonElement.isJsonArray()) {
             JsonArray jsonArray = jsonElement.getAsJsonArray();
-            Iterator<JsonElement> iterator = jsonArray.iterator();
-            while (iterator.hasNext()){
-                parseJson2Map(map,iterator.next().getAsJsonObject(),parentKey);
+            for (int i = 0; i < jsonArray.size(); i++) {
+                parseJson2Map(map, jsonArray.get(i).getAsJsonObject(), parentKey+"_"+i);
             }
-        }else if (jsonElement.isJsonPrimitive()){
+        } else if (jsonElement.isJsonPrimitive()) {
             System.out.println("please check the json format!");
-        }else if (jsonElement.isJsonNull()){
+        } else if (jsonElement.isJsonNull()) {
 
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String json = "{\"code\":200, \"message\":\"ok\", \"data\":\"{\\\"id\\\":131,\\\"appId\\\":6,\\\"versionCode\\\":6014000}\"}";
 
         String test = "{" + "\"hello\": \"sweetzcc\"," +
@@ -118,8 +120,24 @@ public class JsonParseUtil {
                 "\"Time\": \"2018-06-05 10:02:24\"," +
                 "\"sweetzcc\": \"{\\\"needUpdate\\\":true,\\\"Info\\\":\\\"{\\\\\\\"apple\\\\\\\":\\\\\\\"BB199DA64A7692E927722BFD1CA\\\\\\\",\\\\\\\"token\\\\\\\":null,\\\\\\\"uniqueId\\\\\\\":\\\\\\\"868387\\\\\\\",\\\\\\\"pushSweetToken\\\\\\\":\\\\\\\"a968\\\\\\\",\\\\\\\"device\\\\\\\":\\\\\\\"android\\\\\\\",\\\\\\\"systemName\\\\\\\":\\\\\\\"Re\\\\\\\",\\\\\\\"systemV\\\\\\\":\\\\\\\"7.0\\\\\\\",\\\\\\\"pVersion\\\\\\\":\\\\\\\"4.9\\\\\\\",\\\\\\\"key\\\\\\\":\\\\\\\"63e78ea58\\\\\\\",\\\\\\\"chan\\\\\\\":\\\\\\\"net\\\\\\\",\\\\\\\"push\\\\\\\":\\\\\\\"4\\\\\\\",\\\\\\\"userName\\\\\\\":null,\\\\\\\"product\\\\\\\":\\\\\\\"sweet\\\\\\\",\\\\\\\"crime\\\\\\\":1528,\\\\\\\"update1\\\\\\\":15281}\\\"}\"" +
                 "}";
-        String  array = "{'name':'111','child':[{'child':[{'name':'333'}]},{'name':'2221'}]}" ;
-        System.out.println(jsonFormatter(test));
+        String array = "{'name':'111','child':[{'child':[{'name':'333'}]},{'name':'2221'}]}";
+//        System.out.println(jsonFormatter(test));
+
+
+        SingleQuestionVO singleQuestionVO = new SingleQuestionVO();
+        ArrayList<SingleOption> singleOptions = new ArrayList<>();
+        SingleOption op1 = new SingleOption();
+        op1.setContent("op1");
+        SingleOption op2 = new SingleOption();
+        op2.setContent("op2");
+        singleOptions.add(op1);
+        singleOptions.add(op2);
+
+        singleQuestionVO.setOptions(singleOptions);
+        System.out.println(jsonFormatter(JsonUtils.writeValue(singleQuestionVO)));
+
+
+
     }
 
 }
